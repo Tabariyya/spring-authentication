40264b0cdcb8c36506ef47c4ab923896545c6236
waleedshanaa <waleed_shanaa@icloud.com>
2026-05-12 23:33:22 +0300

    removed ldap

--- src/main/java/com/tabariyya/authentication/models/BaseUser.java
+++ src/main/java/com/tabariyya/authentication/models/BaseUser.java
@@ -1,26 +1,25 @@
 package com.tabariyya.authentication.models;
 
-import java.time.LocalDateTime;
 
 public interface BaseUser {
 
     Object getId();
+
     void setId(Object id);
 
     // String userName
     String getUserName();
+
     void setUserName(String userName);
 
     // String password
     String getPassword();
+
     void setPassword(String password);
 
     // String email
     String getEmail();
-    void setEmail(String email);
 
-    // LocalDateTime createdAt
-    LocalDateTime getCreatedAt();
-    void setCreatedAt(LocalDateTime createdAt);
+    void setEmail(String email);
 
 }
